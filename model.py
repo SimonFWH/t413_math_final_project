acea5b9f93b450ebe0c1e9f8d9334a3c177b6e02
maelam-t431 <101439274@georgebrown.ca>
2023-08-04 21:04:16 -0400

    add model

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.model = nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(32 * 64 * 64, 128),
+        nn.ReLU(),
+        nn.Linear(128, 4)
+        )
+
+    def forward(self, x):
+        return self.model(x)
