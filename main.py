d91301718e9942ccfeda68cdcb1290f230e76123
maelam-t431 <101439274@georgebrown.ca>
2023-08-06 20:36:57 -0400

    add interactive loss graph

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import inference
 import torch
 
 if __name__ == "__main__":
-    train_path = "data/train"
+    train_path = "data/test"
     valid_path = "data/valid"
     test_path = "data/test"
-    # train_dataloader = utils.load_images_from_folder(train_path)
-    # valid_dataloader = utils.load_images_from_folder(valid_path)
+    train_dataloader = utils.load_images_from_folder(train_path)
+    valid_dataloader = utils.load_images_from_folder(valid_path)
     model = model.Model()
-    # train.train(model, train_dataloader,valid_dataloader, hyperparameters.num_epochs)
+    train.train(model, train_dataloader,valid_dataloader, hyperparameters.num_epochs)
     # utils.save_model(model)
     model.load_state_dict(torch.load("model/model.pth"))
     inference.infer(model, test_path)
--- train.py
+++ train.py
@@ -3,13 +3,21 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import hyperparameters
+import matplotlib.pyplot as plt
 
 def train(model, train_dataloader, valid_dataloader, num_epochs):
     criterion = nn.SmoothL1Loss()
     optimizer = optim.Adam(model.parameters(), lr=hyperparameters.lr)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
-    
+
+    prev_train = 0
+    prev_valid = 0
+
+    plt.ion()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0.0
@@ -30,6 +38,8 @@ def train(model, train_dataloader, valid_dataloader, num_epochs):
             # Accumulate loss
             train_loss += loss.item()
         
+        train_loss /= i+1
+
         model.eval()
         with torch.no_grad():
             valid_loss = 0.0
@@ -44,7 +54,23 @@ def train(model, train_dataloader, valid_dataloader, num_epochs):
                 # Accumulate loss
                 valid_loss += loss.item()
 
-        # Print epoch statistics
-        train_loss /= i+1
         valid_loss /= j+1
+
         print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {train_loss:.4f}, Validation Loss: {valid_loss:.4f}')
+
+        # Plot the training and validation loss for each epoch
+        if epoch != 0:
+            plt.plot([epoch - 1, epoch],
+                    [prev_train, train_loss],
+                    color="green",
+                    label="Train Loss" if epoch==1 else "")
+            plt.plot([epoch - 1, epoch],
+                    [prev_valid, valid_loss],
+                    color="red",
+                    label="Validation Loss" if epoch==1 else "")
+            plt.pause(0.1)
+            plt.legend()
+            plt.show()
+        
+        prev_train = train_loss
+        prev_valid = valid_loss
